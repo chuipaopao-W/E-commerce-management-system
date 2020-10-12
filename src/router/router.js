7b7311e571a170181d563246dfe5364c79c4a458
wyq <809839231@qq.com>
2020-10-12 11:00:28 +0800

    完成了报表

--- src/router/router.js
+++ src/router/router.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 
 
@@ -20,7 +21,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home,redirect:'/welcome', children:[{path:'/welcome', component: Welcome},{path:'/users',component:Users},{path:'/rights',component:Rights},{path:'/roles',component: Roles},{path:'/categories',component:Cate},{path:'/params',component:Params},{path:'/goods',component:List},{path:'/orders',component:Order}] }
+    { path: '/home', component: Home,redirect:'/welcome', children:[{path:'/welcome', component: Welcome},{path:'/users',component:Users},{path:'/rights',component:Rights},{path:'/roles',component: Roles},{path:'/categories',component:Cate},{path:'/params',component:Params},{path:'/goods',component:List},{path:'/orders',component:Order},{path:'/reports',component:Report}] }
   ]
 })
 
